8a2447cddbb6c80bff4aa7cb3c6c3116311e71d7
Leonardo Schmitt <leoeyeschmittk@gmail.com>
2024-05-12 16:54:25 -0300

    first commit: iniciar projeto com banco de dados instalado

---
+++ src/main/java/dao/FerramentaDAO.java
@@ -0,0 +1,46 @@
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class FerramentaDAO {
+    
+    
+    // conexao com banco de dados // senha "Dgllm2024!"
+     public Connection getConexao() {
+        Connection connection = null; //instância da conexão
+        
+        try {
+
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            String server = "localhost";
+            String database = "db_ferramentas";
+            String url = "jdbc:mysql://" + server + ":3306/"
+                    + database + "?useTimezone=true&serverTimezone=UTC";
+            String user = "root";
+            String password = "Dgllm2024!";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NÃO CONECTADO!");
+            }
+            return connection;
+        } catch (ClassNotFoundException e) { //Driver não encontrado
+            System.out.println("O driver nao foi encontrado.");
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar...");
+            return null;
+        }
+    }
+     
+    
+}
---
+++ src/main/java/principal/Main.java
@@ -0,0 +1,9 @@
+
+package principal;
+
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Está funcionando");
+    }
+}
