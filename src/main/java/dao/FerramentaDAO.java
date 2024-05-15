fc2d65631ac38880dd0b81435699ec75d8d55df6
Leonardo S <leonardosousavargas04@gmail.com>
2024-05-14 21:00:32 -0300

    feat:Adicionada documentação na classe Ferramenta

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class FerramentaDAO {
     
     
--- src/main/java/model/Ferramenta.java
+++ src/main/java/model/Ferramenta.java
@@ -1,6 +1,11 @@
-
 package model;
 
+/**
+ * A classe Ferramenta representa uma ferramenta genérica com nome, marca e custo.
+ * 
+ * @author leona
+ * @version 1.0
+ */
 public class Ferramenta {
     String nome;
     String marca;
@@ -9,33 +14,63 @@ public class Ferramenta {
     public Ferramenta() {
         this("", "", 0.0);
     }
-
+    /**
+     * Construtor que inicializa uma ferramenta com os valores especificados.
+     * 
+     * @param nome  O nome da ferramenta.
+     * @param marca A marca da ferramenta.
+     * @param custo O custo da ferramenta.
+     */
     public Ferramenta(String nome, String marca, double custo) {
         this.nome = nome;
         this.marca = marca;
         this.custo = custo;
     }
-
+    /**
+     * Obtém o nome da ferramenta.
+     * 
+     * @return O nome da ferramenta.
+     */
     public String getNome() {
         return nome;
     }
-
+    /**
+     * Define o nome da ferramenta.
+     * 
+     * @param nome O novo nome da ferramenta.
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    /**
+     * Obtém a marca da ferramenta.
+     * 
+     * @return A marca da ferramenta.
+     */
     public String getMarca() {
         return marca;
     }
-
+    /**
+     * Define a marca da ferramenta.
+     * 
+     * @param marca A nova marca da ferramenta.
+     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
-
+    /**
+     * Obtém o custo da ferramenta.
+     * 
+     * @return O custo da ferramenta.
+     */
     public double getCusto() {
         return custo;
     }
-
+    /**
+     * Define o custo da ferramenta.
+     * 
+     * @param custo O novo custo da ferramenta.
+     */
     public void setCusto(double custo) {
         this.custo = custo;
     }
