fa9af325fbfcfd9ec41b10323b9d29a639752d40
Leonardo S <leonardosousavargas04@gmail.com>
2024-05-16 18:54:40 -0300

    Feat adicionada a documentação na classe FerramentaDAO

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -1,31 +1,40 @@
-
+/**
+ * A classe FerramentaDAO fornece métodos para acessar e manipular dados 
+ * relacionados a ferramentas em um banco de dados.
+ */
 package dao;
 
-import java.sql.*;
-import java.util.ArrayList;
-import model.Ferramenta;
+    import java.sql.*;
+    import java.util.ArrayList;
+    import model.Ferramenta;
 
-
-public class FerramentaDAO {
+/**
+ * Lista de ferramentas recuperadas do banco de dados.
+ */
+ public class FerramentaDAO {
     
     public ArrayList<Ferramenta> FerramentaLista = new ArrayList<>();
-
+    /**
+     * Recupera a lista de ferramentas do banco de dados.
+     * 
+     * @return ArrayList contendo objetos Ferramenta
+     */
     public ArrayList<Ferramenta> getFerramentaLista(){
         FerramentaLista.clear();
         
-        try{
+            try{
             Statement stmt = this.getConexao().createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM tb_alunos");
             while (res.next()) {
 
-                String nome = res.getString("nome");
-                String marca = res.getString("marca");
-                int custo = res.getInt("custo");
+            String nome = res.getString("nome");
+            String marca = res.getString("marca");
+            int custo = res.getInt("custo");
 
-                Ferramenta ObjetoFerramenta = new Ferramenta(nome, marca, custo);
+            Ferramenta ObjetoFerramenta = new Ferramenta(nome, marca, custo);
 
-                FerramentaLista.add(ObjetoFerramenta);
-            }
+            FerramentaLista.add(ObjetoFerramenta);
+        }
             stmt.close();
 
         }catch(SQLException Erro){
@@ -33,31 +42,37 @@ public class FerramentaDAO {
         }
         return FerramentaLista;
     }
-    
+    /**
+     * Define a lista de ferramentas.
+     * 
+     * @param FerramentaLista ArrayList contendo objetos Ferramenta
+     */
     public void setFerramentaLista(ArrayList<Ferramenta> FerramentaLista){
         this.FerramentaLista = FerramentaLista;
     }
-    
-
-
+    /**
+     * Estabelece uma conexão com o banco de dados.
+     * 
+     * @return Connection objeto representando a conexão com o banco de dados
+     */
     // conexao com banco de dados // senha "Dgllm2024!"
      public Connection getConexao() {
         Connection connection = null; //instância da conexão
         
         try {
 
-            String driver = "com.mysql.cj.jdbc.Driver";
-            Class.forName(driver);
+        String driver = "com.mysql.cj.jdbc.Driver";
+        Class.forName(driver);
 
-            String server = "localhost";
+        String server = "localhost";
             
-              String database = "emprestimoFerramentas"; // nome do database
-            String url = "jdbc:mysql://" + server + ":3306/"
-                    + database + "?useTimezone=true&serverTimezone=UTC";
-            String user = "root";
-            String password = "Dgllm2024!"; // senha do banco de dados
+        String database = "emprestimoFerramentas"; // nome do database
+        String url = "jdbc:mysql://" + server + ":3306/"
+                + database + "?useTimezone=true&serverTimezone=UTC";
+        String user = "root";
+        String password = "Dgllm2024!"; // senha do banco de dados
 
-            connection = DriverManager.getConnection(url, user, password);
+        connection = DriverManager.getConnection(url, user, password);
 
             if (connection != null) {
                 System.out.println("Status: Conectado!");
