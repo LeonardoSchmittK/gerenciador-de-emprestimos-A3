dfa4751b4f6fe8af2423252850e6a7cc70daa464
Leonardo S <leonardosousavargas04@gmail.com>
2024-05-16 19:17:52 -0300

    Feat: Adicionada a documentação na classe Main

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -1,9 +1,16 @@
-
+/**
+ * A classe Main é responsável por iniciar a aplicação e exibir a interface gráfica principal.
+ */
 package principal;
 
 import visao.FrmMainFrame;
 
-public class Main {
+public class Main {/**
+     * O método principal da aplicação, que inicia a execução do programa.
+     * Cria uma instância da classe FrmMainFrame e torna a janela visível.
+     * 
+     * @param args argumentos da linha de comando (não utilizado neste caso)
+     */
     public static void main(String[]args){
        FrmMainFrame objetotela = new FrmMainFrame();
         objetotela.setVisible(true);
