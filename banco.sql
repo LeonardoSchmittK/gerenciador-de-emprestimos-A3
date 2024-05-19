9e66f4a7162a115feec8f9cd4ddce4a999d8d259
Leonardo Schmitt <66500082+LeonardoSchmittK@users.noreply.github.com>
2024-05-19 17:05:50 -0300

    feat: atualizando banco.sql com novos comandos

--- banco.sql
+++ banco.sql
@@ -1,64 +1,47 @@
--- MySQL Workbench Forward Engineering
+# criar as 3 tabelas do projeto
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+# criar tabela tb_ferramenta
+CREATE TABLE IF NOT EXISTS tb_ferramenta (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    marca VARCHAR(255),
+    custo DOUBLE
+);
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema db_ferramentas
--- -----------------------------------------------------
+# criar tabela tb_amigo
+CREATE TABLE IF NOT EXISTS tb_ferramenta (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    telefone VARCHAR(255)
+);
 
--- -----------------------------------------------------
--- Schema db_ferramentas
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `db_ferramentas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `db_ferramentas` ;
+# criar tabela tb_emprestimo
+CREATE TABLE IF NOT EXISTS tb_ferramenta (
+    id INT PRIMARY KEY,
+    amigoId INT,
+    ferramentaId INT
+);
 
--- -----------------------------------------------------
--- Table `db_ferramentas`.`tb_amigos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_ferramentas`.`tb_amigos` (
-  `nome` VARCHAR(100) NOT NULL,
-  `telefone` INT NULL,
-  PRIMARY KEY (`nome`))
-ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `db_ferramentas`.`tb_ferramentas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_ferramentas`.`tb_ferramentas` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(100) NULL DEFAULT NULL,
-  `marca` VARCHAR(50) NULL DEFAULT NULL,
-  `custo` INT NULL DEFAULT NULL,
-  `tb_amigos_nome` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_tb_ferramentas_tb_amigos_idx` (`tb_amigos_nome` ASC) VISIBLE,
-  CONSTRAINT `fk_tb_ferramentas_tb_amigos`
-    FOREIGN KEY (`tb_amigos_nome`)
-    REFERENCES `db_ferramentas`.`tb_amigos` (`nome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+# imprimir os dados das 3 tabelas
+SELECT * FROM tb_ferramenta;
+SELECT * FROM tb_amigo;
+SELECT * FROM tb_emprestimo;
 
--- -----------------------------------------------------
--- Table `db_ferramentas`.`tb_emprestimos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_ferramentas`.`tb_emprestimos` (
-  `tb_amigos_nome` VARCHAR(100) NOT NULL,
-  INDEX `fk_tb_emprestimos_tb_amigos1_idx` (`tb_amigos_nome` ASC) VISIBLE,
-  CONSTRAINT `fk_tb_emprestimos_tb_amigos1`
-    FOREIGN KEY (`tb_amigos_nome`)
-    REFERENCES `db_ferramentas`.`tb_amigos` (`nome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+# reordenar ids ao remover ferramenta, não obrigatório
+SET @new_id = 0;
+UPDATE tb_ferramenta
+SET id = (@new_id := @new_id + 1)
+ORDER BY id;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+# deletar tabela tb_ferramenta
+DROP TABLE tb_ferramenta;
+
+# deletar tabela tb_amigo
+DROP TABLE tb_amigo;
+
+# deletar tabela tb_emprestimo
+DROP TABLE tb_emprestimo;
+
+# habilitar safe mode no workbench para deletes e updates
+SET SQL_SAFE_UPDATES = 0;
