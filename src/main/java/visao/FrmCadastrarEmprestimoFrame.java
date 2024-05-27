c811eec5c6518f0856cca2ae620d43a61e848f07
Leonardo Schmitt <leoeyeschmittk@gmail.com>
2024-05-27 13:09:34 -0300

    feat: adicionando input de dias na tela de cadastro de empréstimo

--- src/main/java/visao/FrmCadastrarEmprestimoFrame.java
+++ src/main/java/visao/FrmCadastrarEmprestimoFrame.java
@@ -11,16 +11,17 @@ import model.Ferramenta;
 public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
 
     private Ferramenta objetoFerramenta;
-    private Amigo objetoAmigo;    
+    private Amigo objetoAmigo;
     private Emprestimo objetoEmprestimo;
 
+    private String dateEmprestimoFinal;
+    private String dateEmprestimoInicial;
     private boolean temAmigo;
     private boolean temFerramenta;
     private String nomeEscolhido;
     private String ferramentaEscolhida;
     public ArrayList<Ferramenta> FerramentaLista = new ArrayList<>();
     public ArrayList<Amigo> AmigoLista = new ArrayList<>();
-    
 
     public FrmCadastrarEmprestimoFrame() {
         initComponents();
@@ -29,15 +30,15 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
         this.objetoEmprestimo = new Emprestimo();
         this.objetoFerramenta = new Ferramenta();
         this.objetoAmigo = new Amigo();
-        
-        this.FerramentaLista = objetoFerramenta.getListaFerramentas();       
+
+        this.FerramentaLista = objetoFerramenta.getListaFerramentas();
         this.AmigoLista = objetoAmigo.getListaAmigo();
 
-        
         this.imprimirFerramentas();
         this.imprimirAmigos();
-        this.imprimirValidadeEmprestimo();
+        this.imprimirValidadeEmprestimo(15);
         this.imprimirEmprestimoCoibido();
+
     }
 
     @SuppressWarnings("unchecked")
@@ -52,6 +53,9 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
         jComboBoxSelecionarFerramenta = new javax.swing.JComboBox<>();
         jComboBoxSelecionarAmigo = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jQuantidadeDiasEmprestimo = new javax.swing.JTextField();
         jPanelPainelBotoesEmprestimo = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
@@ -90,6 +94,45 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
 
         jLabel2.setText("Emprestar");
 
+        jLabel4.setText("por");
+
+        jLabel5.setText("dias");
+
+        jQuantidadeDiasEmprestimo.setText("15");
+        jQuantidadeDiasEmprestimo.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                jQuantidadeDiasEmprestimoFocusLost(evt);
+            }
+        });
+        jQuantidadeDiasEmprestimo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jQuantidadeDiasEmprestimoMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jQuantidadeDiasEmprestimoMouseExited(evt);
+            }
+        });
+        jQuantidadeDiasEmprestimo.addInputMethodListener(new java.awt.event.InputMethodListener() {
+            public void caretPositionChanged(java.awt.event.InputMethodEvent evt) {
+            }
+            public void inputMethodTextChanged(java.awt.event.InputMethodEvent evt) {
+                jQuantidadeDiasEmprestimoInputMethodTextChanged(evt);
+            }
+        });
+        jQuantidadeDiasEmprestimo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jQuantidadeDiasEmprestimoActionPerformed(evt);
+            }
+        });
+        jQuantidadeDiasEmprestimo.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jQuantidadeDiasEmprestimoKeyPressed(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jQuantidadeDiasEmprestimoKeyTyped(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanelPainelEmprestimoLayout = new javax.swing.GroupLayout(jPanelPainelEmprestimo);
         jPanelPainelEmprestimo.setLayout(jPanelPainelEmprestimoLayout);
         jPanelPainelEmprestimoLayout.setHorizontalGroup(
@@ -103,7 +146,13 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jComboBoxSelecionarAmigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel4)
+                .addGap(13, 13, 13)
+                .addComponent(jQuantidadeDiasEmprestimo, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5)
+                .addContainerGap(44, Short.MAX_VALUE))
         );
         jPanelPainelEmprestimoLayout.setVerticalGroup(
             jPanelPainelEmprestimoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -113,8 +162,11 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
                     .addComponent(jLabel2)
                     .addComponent(jLabel3)
                     .addComponent(jComboBoxSelecionarFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBoxSelecionarAmigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                    .addComponent(jComboBoxSelecionarAmigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(jQuantidadeDiasEmprestimo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(25, Short.MAX_VALUE))
         );
 
         jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
@@ -133,7 +185,7 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
             }
         });
 
-        jLabelValidadeEmprestimo.setText(" O empréstimo tem validade de 15 dias ");
+        jLabelValidadeEmprestimo.setText(" O empréstimo tem validade padrão de 15 dias ");
 
         javax.swing.GroupLayout jPanelPainelBotoesEmprestimoLayout = new javax.swing.GroupLayout(jPanelPainelBotoesEmprestimo);
         jPanelPainelBotoesEmprestimo.setLayout(jPanelPainelBotoesEmprestimoLayout);
@@ -175,11 +227,11 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(14, 14, 14)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanelPainelEmprestimo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jPanelPainelBotoesEmprestimo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabelAlertaEmprestimoCoibido)
-                    .addComponent(jLabel1))
-                .addContainerGap(105, Short.MAX_VALUE))
+                    .addComponent(jLabel1)
+                    .addComponent(jPanelPainelEmprestimo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(197, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -190,9 +242,9 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
                 .addComponent(jLabelAlertaEmprestimoCoibido)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanelPainelEmprestimo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(21, 21, 21)
+                .addGap(52, 52, 52)
                 .addComponent(jPanelPainelBotoesEmprestimo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(86, Short.MAX_VALUE))
+                .addContainerGap(36, Short.MAX_VALUE))
         );
 
         pack();
@@ -233,37 +285,80 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(rootPane, "Escolha as opções corretamente!");
 
         } else {
+
             int idxEscolhidoFerramenta = jComboBoxSelecionarFerramenta.getSelectedIndex();
             int idxEscolhidoAmigo = jComboBoxSelecionarAmigo.getSelectedIndex();
 
-            Ferramenta ferramentaEscolhida = this.FerramentaLista.get(idxEscolhidoFerramenta-1);
-            Amigo amigoEscolhido = this.AmigoLista.get(idxEscolhidoAmigo-1);
+            Ferramenta ferramentaEscolhida = this.FerramentaLista.get(idxEscolhidoFerramenta - 1);
+            Amigo amigoEscolhido = this.AmigoLista.get(idxEscolhidoAmigo - 1);
 
             int idFerramenta = ferramentaEscolhida.getId();
             String nomeFerramenta = ferramentaEscolhida.getNome();
             int idAmigo = amigoEscolhido.getId();
             String nomeAmigo = amigoEscolhido.getNome();
-                
-            if(this.objetoEmprestimo.estaAmigoDevendo(idAmigo)){
-                JOptionPane.showMessageDialog(rootPane,  nomeAmigo + " tem empréstimos não devolvidos!");
+
+            if (this.objetoEmprestimo.estaAmigoDevendo(idAmigo)) {
+                JOptionPane.showMessageDialog(rootPane, nomeAmigo + " tem empréstimos não devolvidos!");
             }
             
-            if (this.objetoEmprestimo.insertEmprestimoDb(idFerramenta, nomeFerramenta, idAmigo, nomeAmigo))  {
+            String dataFinal = this.getDateEmprestimoFinal();
+            String dataInicial = this.getDateEmprestimoInicial();
+
+            if (this.objetoEmprestimo.insertEmprestimoDb(idFerramenta, nomeFerramenta, idAmigo, nomeAmigo)) {
                 JOptionPane.showMessageDialog(rootPane, "Emprestimo cadastrado com sucesso!");
                 // limpa campos da interface
                 jComboBoxSelecionarFerramenta.setSelectedIndex(0);
                 jComboBoxSelecionarAmigo.setSelectedIndex(0);
+                jQuantidadeDiasEmprestimo.setText("15");
+                this.imprimirValidadeEmprestimo(15);
 
             }
         }
 
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    
+    private void jQuantidadeDiasEmprestimoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jQuantidadeDiasEmprestimoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jQuantidadeDiasEmprestimoActionPerformed
 
-    
+    private void jQuantidadeDiasEmprestimoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jQuantidadeDiasEmprestimoKeyTyped
 
-    
+    }//GEN-LAST:event_jQuantidadeDiasEmprestimoKeyTyped
+
+    private void jQuantidadeDiasEmprestimoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jQuantidadeDiasEmprestimoKeyPressed
+
+
+    }//GEN-LAST:event_jQuantidadeDiasEmprestimoKeyPressed
+
+    private void jQuantidadeDiasEmprestimoFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jQuantidadeDiasEmprestimoFocusLost
+
+
+    }//GEN-LAST:event_jQuantidadeDiasEmprestimoFocusLost
+
+    private void jQuantidadeDiasEmprestimoInputMethodTextChanged(java.awt.event.InputMethodEvent evt) {//GEN-FIRST:event_jQuantidadeDiasEmprestimoInputMethodTextChanged
+
+
+    }//GEN-LAST:event_jQuantidadeDiasEmprestimoInputMethodTextChanged
+
+    private void jQuantidadeDiasEmprestimoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jQuantidadeDiasEmprestimoMouseExited
+        jQuantidadeDiasEmprestimo.setFocusable(false);
+
+        try {
+            int labelDate = Integer.parseInt(jQuantidadeDiasEmprestimo.getText());
+            this.imprimirValidadeEmprestimo(labelDate);
+
+        } catch (NumberFormatException exception) {
+            JOptionPane.showConfirmDialog(rootPane, "Digite um número");
+            jQuantidadeDiasEmprestimo.setText("15");
+        }
+
+
+    }//GEN-LAST:event_jQuantidadeDiasEmprestimoMouseExited
+
+    private void jQuantidadeDiasEmprestimoMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jQuantidadeDiasEmprestimoMouseEntered
+        // TODO add your handling code here:
+        jQuantidadeDiasEmprestimo.setFocusable(true);
+    }//GEN-LAST:event_jQuantidadeDiasEmprestimoMouseEntered
 
     public void imprimirEmprestimoCoibido() {
         if (!this.isTemAmigo() || !this.isTemFerramenta()) {
@@ -300,12 +395,19 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
 
     }
 
-    public void imprimirValidadeEmprestimo() {
-        int quantidadeDiasEmprestimo = 15;
+    public void imprimirValidadeEmprestimo(int dias) {
+        int quantidadeDiasEmprestimo = dias;
         LocalDate date = LocalDate.now();
         LocalDate dateEmprestimo = LocalDate.now().plusDays(quantidadeDiasEmprestimo);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        jLabelValidadeEmprestimo.setText("Validade do empréstimo: até " + dateEmprestimo.format(formatter));
+        String[] daysOfWeekPTBR = {"domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"};
+        String formattedDateEmprestimo = daysOfWeekPTBR[dateEmprestimo.getDayOfWeek().getValue()] + ", " + dateEmprestimo.format(formatter);
+        String dateEmprestimoFinal = dateEmprestimo.format(formatter);
+        this.setDateEmprestimoFinal(dateEmprestimoFinal);
+        this.setDateEmprestimoInicial(date.format(formatter));
+
+        jLabelValidadeEmprestimo.setText(
+                "O empréstimo terá validade até " + formattedDateEmprestimo);
     }
 
     /**
@@ -322,16 +424,24 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmCadastrarEmprestimoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCadastrarEmprestimoFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmCadastrarEmprestimoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCadastrarEmprestimoFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmCadastrarEmprestimoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCadastrarEmprestimoFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmCadastrarEmprestimoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCadastrarEmprestimoFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -375,6 +485,22 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
         this.ferramentaEscolhida = ferramentaEscolhida;
     }
 
+    public String getDateEmprestimoFinal() {
+        return dateEmprestimoFinal;
+    }
+
+    public void setDateEmprestimoFinal(String dateEmprestimoFinal) {
+        this.dateEmprestimoFinal = dateEmprestimoFinal;
+    }
+
+    public String getDateEmprestimoInicial() {
+        return dateEmprestimoInicial;
+    }
+
+    public void setDateEmprestimoInicial(String dateEmprestimoInicial) {
+        this.dateEmprestimoInicial = dateEmprestimoInicial;
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
@@ -384,10 +510,13 @@ public class FrmCadastrarEmprestimoFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabelAlertaEmprestimoCoibido;
     private javax.swing.JLabel jLabelValidadeEmprestimo;
     private javax.swing.JPanel jPanelPainelBotoesEmprestimo;
     private javax.swing.JPanel jPanelPainelEmprestimo;
+    private javax.swing.JTextField jQuantidadeDiasEmprestimo;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 
